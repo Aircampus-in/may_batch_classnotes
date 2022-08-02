88e67ffec661b246ec9820ed0792a145d01dad7e
vasanthkumar10 <vasizebron10@gmail.com>
2022-08-02 21:52:55 +0530

    calculator

--- about.js
+++ about.js
@@ -1,42 +1,77 @@
-const grandParent = document.querySelector(".grandparent");
-const parent = document.querySelector(".parent");
-const child = document.querySelector(".child");
-const btn = document.querySelector(".btn");
+// const grandParent = document.querySelector(".grandparent");
+// const parent = document.querySelector(".parent");
+// const child = document.querySelector(".child");
+// const btn = document.querySelector(".btn");
 
-function printGrandParent(event) {
-  console.log("grand parent bubbling", event.target);
+// function printGrandParent(event) {
+//   console.log("grand parent bubbling", event.target);
+// }
+
+// function printOnHover() {
+//   console.log("hovering");
+// }
+
+// grandParent.addEventListener("click", printGrandParent);
+
+// function removeGrandParent() {
+//   setTimeout(() => {
+//     console.log("removing grandparent");
+//     grandParent.removeEventListener("click", printGrandParent);
+//   }, 2000);
+// }
+
+// btn.addEventListener("click", removeGrandParent);
+
+// parent.addEventListener("click", (event) => {
+//   console.log("parent bubbling");
+// });
+
+// child.addEventListener("click", (event) => {
+//   console.log("child bubbling");
+// });
+
+// const newDiv = document.createElement("div");
+// newDiv.style.width = "10rem";
+// newDiv.style.height = "10rem";
+// newDiv.style.backgroundColor = "red";
+// const para = document.createElement("p");
+// para.innerText = "vasanthkumar ";
+// newDiv.append(para);
+// document.body.append(newDiv);
+
+// child.append(para);
+
+function test() {
+  this.style.backgroundColor = "red";
+  // console.log(document.querySelector(".btn"));
 }
 
-function printOnHover() {
-  console.log("hovering");
-}
+// // test();
+// // grandParent.addEventListener("click", test);
+// btn.addEventListener("click", test);
+// child.addEventListener("click", test);
 
-grandParent.addEventListener("click", printGrandParent);
+// console.log(eval(10 * 5 + 5 / 2));
 
-function removeGrandParent() {
-  setTimeout(() => {
-    console.log("removing grandparent");
-    grandParent.removeEventListener("click", printGrandParent);
-  }, 2000);
-}
+// console.log(8 + 2);
 
-btn.addEventListener("click", removeGrandParent);
+// console.log("hello".indexOf("i", 3));
 
-parent.addEventListener("click", (event) => {
-  console.log("parent bubbling");
-});
+// String.prototype.vasanthIndex = function (target, starting = 0) {
+//   console.log(this);
+//   for (let index = starting; index < this.length; index++) {
+//     if (this[index] === target) {
+//       return index;
+//     }
+//   }
+//   return -1;
+// };
 
-child.addEventListener("click", (event) => {
-  console.log("child bubbling");
-});
+// console.log("hello".vasanthIndex("i"));
 
-const newDiv = document.createElement("div");
-newDiv.style.width = "10rem";
-newDiv.style.height = "10rem";
-newDiv.style.backgroundColor = "red";
-const para = document.createElement("p");
-para.innerText = "vasanthkumar ";
-newDiv.append(para);
-document.body.append(newDiv);
+Array.prototype.vasanthIncludes = function () {
+  console.log(this);
+};
 
-child.append(para);
+[1, 2, 4, 5].vasanthIncludes();
+["dabda", "dada"].vasanthIncludes();
