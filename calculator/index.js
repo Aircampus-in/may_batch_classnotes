2dad91d3b046d52c9d6559f45abb956267ba1b72
vasanthkumar10 <vasizebron10@gmail.com>
2022-08-03 14:18:15 +0530

    calculator project

---
+++ calculator/index.js
@@ -0,0 +1,25 @@
+let display = document.getElementById("display");
+
+let buttons = Array.from(document.getElementsByClassName("btn"));
+
+buttons.map((btn) => {
+  btn.addEventListener("click", (event) => {
+    let key = event.target.innerText;
+
+    if (key == "A/c") {
+      display.innerText = "";
+    } else if (key == "del") {
+      if (display.innerText == "Infinity") {
+        display.innerText = "";
+      } else {
+        display.innerText = display.innerText.slice(0, -1);
+      }
+    } else if (key == "=") {
+      if (display.innerText) {
+        display.innerText = eval(display.innerText);
+      }
+    } else {
+      display.innerText += key;
+    }
+  });
+});
